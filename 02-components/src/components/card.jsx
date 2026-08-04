8cc407470aed439c327e030d2562e529f70cab00
Manan <manannagpal003@gmail.com>
2026-08-04 10:28:40 +0530

    adding the card and navbar

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+    <div className='card'>
+        <h1>WASSUP</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est alias voluptates iusto earum nostrum velit? Similique quo molestiae iure consequuntur quis fuga ipsam nemo expedita sunt, odit consequatur accusamus assumenda.</p>
+     </div>
+    </div>
+  )
+}
+
+export default card
