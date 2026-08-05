9381df6ba30de023395b2fc447d8a7c4515aca02
Manan <manannagpal003@gmail.com>
2026-08-05 13:05:38 +0530

    adding left and right content pages

---
+++ 06-ui-project/src/components/Section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-6 px-16'>
+       <h4 className='bg-black text-white uppercase px-5 py-3 rounded-full tracking-wider'>Target Audience </h4>
+       <button className='bg-gray-400 text-white  uppercase px-5 py-3 rounded-full tracking-wider'> Digital Banking Platform</button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ 06-ui-project/src/components/Section1/Page1Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page1Content = () => {
+  return (
+    <div className='px-18 py-3'>
+        
+    </div>
+  )
+}
+
+export default Page1Content
