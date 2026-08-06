4d02cacb68575f7fe048df15f399b01152cddade
Manan <manannagpal003@gmail.com>
2026-08-06 14:07:39 +0530

    adding rightcardcontent

--- 06-ui-project/src/components/Section1/RightCard.jsx
+++ 06-ui-project/src/components/Section1/RightCard.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import RightCardContent from './RightCardContent'
 
-const RightCard = () => {
-  return (
-    <div className='h-full w-80 overflow-hidden rounded-4xl'>
-        <img className='h-full w-full object-cover'src="https://plus.unsplash.com/premium_photo-1661641353075-f0eaf2d82aae?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-
-
-</div>
-  )
+const RightCard = (props) => {
+    console.log(props.color);
+    
+    return (
+        <div className='h-full shrink-0 overflow-hidden relative w-80 rounded-4xl'>
+            <img className='h-full w-full object-cover' src={props.img} alt="" />
+            <RightCardContent color={props.color} id={props.id} tag={props.tag} />
+        </div>
+    )
 }
 
 export default RightCard
--- 06-ui-project/src/components/Section1/RightContent.jsx
+++ 06-ui-project/src/components/Section1/RightContent.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import 'remixicon/fonts/remixicon.css'
 import RightCard from './RightCard'
 
-const RightContent = () => {
+const RightContent = (props) => {
+  console.log(props.users.color);
+  
   return (
-    <div className='h-full w-2/3 p-6 bg-blue-300 '>
-      <RightCard/>
-      
-     
+    <div id='right' className='h-full flex rounded-4xl overflow-x-auto flex-nowrap gap-10 p-6 w-2/3'>
+        {props.users.map(function(elem,idx){
+
+          return <RightCard key={idx} color={elem.color} id={idx} img={elem.img} tag={elem.tag} />
+        })}
     </div>
   )
 }
