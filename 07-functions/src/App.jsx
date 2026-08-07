fb30a03f639dcd29e1cf252db21e5575d8d11acc
Manan <manannagpal003@gmail.com>
2026-08-07 12:21:50 +0530

    adding the placeholder

--- 07-functions/src/App.jsx
+++ 07-functions/src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 
 const App = () => {
-  function btnClicked(){
-    console.log('clicked');
-  }
-
-  function mouseEnter(){
-    console.log('mouse entered');
-  }
+  
   return (
     <div>
-      <h1>Hello</h1>
-      <button onMouseEnter={mouseEnter} onClick={btnClicked}>change user</button>
+      <input type="text" placeholder="Enter your name"/>
     </div>
   )
 }
