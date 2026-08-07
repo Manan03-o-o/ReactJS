b7d04456df02d03f9223c00f90481f1a9999c003
Manan <manannagpal003@gmail.com>
2026-08-07 12:16:08 +0530

    adding the onclick func

--- 07-functions/src/App.jsx
+++ 07-functions/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const App = () => {
+  function btnClicked(){
+    console.log('clicked');
+  }
+
+  function mouseEnter(){
+    console.log('mouse entered');
+  }
   return (
     <div>
       <h1>Hello</h1>
+      <button onMouseEnter={mouseEnter} onClick={btnClicked}>change user</button>
     </div>
   )
 }
