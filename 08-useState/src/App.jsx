978b9a210ee223cb164494cabb87fed236c69e3d
Manan <manannagpal003@gmail.com>
2026-08-08 12:25:10 +0530

    added username values

--- 08-useState/src/App.jsx
+++ 08-useState/src/App.jsx
@@ -2,10 +2,18 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [num, setNum] = useState(0)
+  const [username, setUserName] = useState('alice')
+  const [users, setUsers] = useState([10, 20, 30, 40])
+
+  function changeNum(){
+    setNum(20)
+    setUserName('bob')
+  }
   return (
     <div>
-      <h1>Value of a is {num}</h1>
-      <button onClick={() => setNum(num + 1)}>click</button>
+      <h1>Value of num is {num}</h1>
+      <p>Username: {username}</p>
+      <button onClick={changeNum}>click</button>
     </div>
   )
 }
