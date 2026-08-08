cc1bf23b54e9d027ba39ea3a7a5e48643aca7127
Manan <manannagpal003@gmail.com>
2026-08-08 12:48:03 +0530

    adding buttons

--- 08-useState/src/App.jsx
+++ 08-useState/src/App.jsx
@@ -1,21 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [num, setNum] = useState(0)
-  const [username, setUserName] = useState('alice')
-  const [users, setUsers] = useState([10, 20, 30, 40])
-
-  function changeNum(){
-    setNum(20)
-    setUserName('bob')
-    setUsers([100, 200, 300, 400])
-
-  }
   return (
     <div>
-      <h1>Value of num is {num} <br />{users.join(', ')}</h1>
-      <p>Username: {username} </p>
-      <button onClick={changeNum}>click</button>
+      <button>increase</button>
+      <button>decrease</button>
     </div>
   )
 }
