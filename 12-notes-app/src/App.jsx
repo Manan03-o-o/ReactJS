5bafc96fba5f17eddcfbe6156c869dd948a6541c
Manan <manannagpal003@gmail.com>
2026-08-11 10:45:38 +0530

    added buttons

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const App = () => {
   return (
     <div className="h-screen bg-black text-white">
-      <form className="flex flex-col gap-4 p-10">
+      <form className="flex items-start flex-col gap-4 p-10">
         <input
           type="text"
           placeholder="Enter Notes Heading"
-          className="px-5 py-2 border-2 rounded"
+          className="px-5 w-1/2 py-2 border-2 rounded"
         />
 
         <input
           type="text"
           placeholder="Write Details"
-          className="px-5 h-20 py-2 border-2 rounded"
+          className="px-5 w-1/2 h-20 py-2 border-2 rounded"
         />
 
-        <button>Add Note</button>
+        <button className='bg-amber-50 w-1/2 text-black px-5 py-2 rounded'>Add Note</button>
       </form>
     </div>
   )
