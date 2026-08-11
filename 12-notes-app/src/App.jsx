10182714b16a3ecf0e7735b4ed1e1e8b8dde25c1
Manan <manannagpal003@gmail.com>
2026-08-11 10:48:52 +0530

    adding notes.png

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -4,7 +4,8 @@ const App = () => {
   return (
     <div className="h-screen bg-black text-white">
       <form className="flex items-start flex-col gap-4 p-10">
-        <input
+       <div>
+         <input
           type="text"
           placeholder="Enter Notes Heading"
           className="px-5 w-1/2 py-2 border-2 rounded"
@@ -17,6 +18,8 @@ const App = () => {
         />
 
         <button className='bg-amber-50 w-1/2 text-black px-5 py-2 rounded'>Add Note</button>
+       </div>
+      <img src="https://www.bing.com/th/id/OIP.JtTfbqMIHwd30CwhagZdkgHaHa?w=193&h=193&c=8&rs=1&qlt=90&r=0&o=6&dpr=1.3&pid=ImgAns&rm=2/" alt="Note Image" />
       </form>
     </div>
   )
