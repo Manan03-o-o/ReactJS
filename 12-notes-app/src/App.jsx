c664e20d14a0d43de6d689f099a76bbf13354879
Manan <manannagpal003@gmail.com>
2026-08-11 10:32:05 +0530

    fixed css

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='h-screen bg-gray-950 text-white'>
-      <form className='felx flex-col gap-4 p-10'>
-        <input type="text" placeholder="Enter Notes Heading"
-        className='px-5 py-2 border-2 rounded' />
-       <input type ="text" className='px-5 py-2 border-rounded' placeholder="Enter Details" />
+    <div className="h-screen bg-black text-white">
+      <form className="flex flex-col gap-4 p-10">
+        <input
+          type="text"
+          placeholder="Enter Notes Heading"
+          className="px-5 py-2 border-2 rounded"
+        />
+
+        <input
+          type="text"
+          placeholder="Write Details"
+          className="px-5 h-20 py-2 border-2 rounded"
+        />
+
+        <button>Add Note</button>
       </form>
     </div>
   )
