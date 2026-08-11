c3554dc2ed09cf8e5a187b1010831c2a2778a9bd
Manan <manannagpal003@gmail.com>
2026-08-11 11:44:05 +0530

    adding form handling

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 
 const App = () => {
+  const submitHandler = (e) => {
+    e.preventDefault()
+   console.log("form submitted")
+  }
   return (
     <div className="h-screen bg-black text-white">
-      <form className="flex items-start flex-col gap-4 p-10">
-       <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} className="flex justify-between items-start   p-10">
+       <div className='flex items-start w-1/2 flex-col gap-4'>
          <input
           type="text"
           placeholder="Enter Notes Heading"
-          className="px-5 w-1/2 py-2 border-2 rounded"
+          className="px-5  py-2  w-full font-medium border-2 rounded"
         />
 
-        <input
+        <textarea
           type="text"
           placeholder="Write Details"
-          className="px-5 w-1/2 h-20 py-2 border-2 rounded"
+          className="px-5 w-full h-40 py-2 font-medium border-2 rounded"
         />
 
-        <button className='bg-amber-50 w-1/2 text-black px-5 py-2 rounded'>Add Note</button>
+        <button className='bg-amber-50 w-full font-medium  text-black px-5 py-2 rounded'>Add Note</button>
        </div>
-      <img src="https://www.bing.com/th/id/OIP.JtTfbqMIHwd30CwhagZdkgHaHa?w=193&h=193&c=8&rs=1&qlt=90&r=0&o=6&dpr=1.3&pid=ImgAns&rm=2/" alt="Note Image" />
       </form>
     </div>
   )
