a50a3920bd462cf7b5c6d44fdf34f2bf0a256456
Manan <manannagpal003@gmail.com>
2026-08-11 10:24:25 +0530

    adding properties

--- 12-notes-app/src/App.jsx
+++ 12-notes-app/src/App.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen bg-gray-950 text-white'>
+      <form className='felx flex-col gap-4 p-10'>
+        <input type="text" placeholder="Enter Notes Heading"
+        className='px-5 py-2 border-2 rounded' />
+       <input type ="text" className='px-5 py-2 border-rounded' placeholder="Enter Details" />
+      </form>
+    </div>
   )
 }
 
