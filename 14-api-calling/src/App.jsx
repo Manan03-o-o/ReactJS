5d463da75e3805a3915bd7ee17577e26371ae1a6
Manan <manannagpal003@gmail.com>
2026-08-14 22:22:08 +0530

    imported axios

--- 14-api-calling/src/App.jsx
+++ 14-api-calling/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
 
-  const getData = async ()=>{
-    const response =await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    console.log(response)
+  const getData = ()=>{
+
   }
   return (
     <div>
