734e6706dc4e7133044e740f2e02e3ef09d5b8e8
Manan <manannagpal003@gmail.com>
2026-08-15 22:40:18 +0530

    fetching data

--- 16-gallery-project/src/App.jsx
+++ 16-gallery-project/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const getData = ()=>{
+    console.log('data aa gaya');
+
+  }
   return (
     <div className='bg-black h-screen text-white'>
-      <button className='bg-green-600 px-5 py-2 rounded text-white'>get data</button>
+      <button
+      onClick={getData}
+       className='bg-green-600 px-5 py-2 mb-2 active:scale-95 rounded text-white'>get data</button>
     </div>
   )
 }
