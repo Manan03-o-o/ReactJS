08fedb2faebff6454dce5cf57eddb2b1ab977295
Manan <manannagpal003@gmail.com>
2026-08-15 22:35:07 +0530

    adding functionalities

--- 16-gallery-project/src/App.jsx
+++ 16-gallery-project/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div></div>
+    <div className='bg-black h-screen text-white'>
+      <button className='bg-green-600 px-5 py-2 rounded text-white'>get data</button>
+    </div>
   )
 }
 
